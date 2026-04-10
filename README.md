cb562825de9c5f17e6a8f4a95dd331552f8fd111
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-10 07:27:38 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Miologia PWA
+
+App PWA com o roteiro prático de Anatomia Humana - Miologia.
+
+## Arquivos
+- index.html
+- manifest.json
+- sw.js
+- assets/icons
+
+## Publicação
+Suba o conteúdo no GitHub Pages.
--- sw.js
+++ sw.js
@@ -1,13 +1,8 @@
 
-const CACHE = "roteiro-miologia-v5";
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll([
-      "./","./index.html","./css/style.css","./js/data.js","./js/app.js","./manifest.json",
-      "./assets/icons/icon192.png","./assets/icons/icon512.png"
-    ]))
-  );
+const CACHE='miologia-pwa-v1';
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/icons/icon192.png','./assets/icons/icon512.png'])));
 });
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
